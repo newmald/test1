984c9e840245cc6f6de0b7b4dbfb1e85539750a6
Aleksey Kalmychkov <mald-online@yandex.ru>
2018-07-21 00:41:27 +0300

    add project

---
+++ scripts.js
@@ -0,0 +1,15 @@
+var callback = document.querySelector('.callback');
+var modal = document.querySelector('.modal');
+var overlay = document.querySelector('.overlay');
+var close = document.querySelector('.modal__close');
+
+callback.addEventListener('click', function() {
+	modal.classList.add('show');
+	overlay.classList.add('show');
+});
+
+close.addEventListener('click', function() {
+	modal.classList.remove('show');
+	overlay.classList.remove('show');
+});
+/*пываерувкырпим*/
